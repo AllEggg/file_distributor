d519481924aa502830feece699ef0007efe22b58
PC <o.s.ryabchenkov@gmail.com>
2024-01-13 23:18:27 +0300

    try - catch main

--- src/main/java/ru/oleaghue/file_distributor/FileDistributorApplication.java
+++ src/main/java/ru/oleaghue/file_distributor/FileDistributorApplication.java
@@ -15,27 +15,31 @@ import java.util.concurrent.TimeUnit;
 public class FileDistributorApplication {
 
 	public static void main(String[] args) throws InterruptedException {
+		try {
+			SpringApplication.run(FileDistributorApplication.class, args);
+			String appDir = System.getProperty("user.dir");
+			OutLogWriter.startLogWriter(appDir);
+			Map<String, String> settingsMap = ConfigReader.readConfig(appDir);
+			String baseDir = settingsMap.get("BaseDirectory");
+			String newDir = settingsMap.get("DirectoryToDistribute");
 
-		SpringApplication.run(FileDistributorApplication.class, args);
-		String appDir = System.getProperty("user.dir");
-		OutLogWriter.startLogWriter(appDir);
-		Map<String, String> settingsMap = ConfigReader.readConfig(appDir);
-		String baseDir = settingsMap.get("BaseDirectory");
-		String newDir = settingsMap.get("DirectoryToDistribute");
-
-		FileDistributor distributor = new FileDistributor();
-		boolean needStop = false;
-		while (!needStop) {
-			LocalDateTime dateOfIteration = LocalDateTime.now();
-			System.out.println(dateOfIteration + " Запущен процесс копирования");
-			distributor.distribute(baseDir, newDir);
-			System.out.println(dateOfIteration + " Копирование завершено");
-			Thread.sleep(TimeUnit.HOURS.toMillis(1L));
-			settingsMap = ConfigReader.readConfig(appDir);
-			needStop = Objects.equals("YES", settingsMap.get("NeedToStop"));
+			FileDistributor distributor = new FileDistributor();
+			boolean needStop = false;
+			while (!needStop) {
+				LocalDateTime dateOfIteration = LocalDateTime.now();
+				System.out.println(dateOfIteration + " Запущен процесс копирования");
+				distributor.distribute(baseDir, newDir);
+				System.out.println(dateOfIteration + " Копирование завершено");
+				Thread.sleep(TimeUnit.HOURS.toMillis(1L));
+				settingsMap = ConfigReader.readConfig(appDir);
+				needStop = Objects.equals("YES", settingsMap.get("NeedToStop"));
+			}
+			LocalDateTime date = LocalDateTime.now();
+			System.out.println(date + "Приложение завершило работу по команде из xml файла.");
+		} catch (Exception e) {
+			System.out.println(e.getMessage() + "\n" + e.getCause());
+			throw e;
 		}
-		LocalDateTime date = LocalDateTime.now();
-		System.out.println(date + "Приложение завершило работу по команде из xml файла.");
 	}
 
 }
