a3aa85f8fb492c86514924a658bea64b0e2c090f
PC <o.s.ryabchenkov@gmail.com>
2024-01-17 22:07:31 +0300

    Запуск заданий в цикле

--- src/main/java/ru/oleaghue/file_distributor/FileDistributorApplication.java
+++ src/main/java/ru/oleaghue/file_distributor/FileDistributorApplication.java
@@ -8,6 +8,7 @@ import ru.oleaghue.file_distributor.util.FileDistributor;
 import ru.oleaghue.file_distributor.util.OutLogWriter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ExecutorService;
@@ -25,102 +26,51 @@ public class FileDistributorApplication {
 			OutLogWriter.startLogWriter(appDir);
 			ExecutorService executorService = Executors.newCachedThreadPool();
 
-			Thread t1 = new Thread(() -> {
-				FileDistributor distributor = new FileDistributor();
-				String studioLabel = "Studio1";
-				Map<String, String> settingsMap = ConfigReader.readConfig(appDir, studioLabel);
-				Configuration config = new Configuration(settingsMap);
-				boolean needStop = false;
-				while (!needStop) {
-					long sleepTime;
-					System.out.println(LocalDateTime.now() + " Запущен процесс копирования " + studioLabel);
-					distributor.distribute(config.getBaseDir(), config.getNewDir());
-					System.out.println(LocalDateTime.now() + " Копирование завершено " + studioLabel);
-					try {
-						sleepTime = Long.parseLong(config.getSleepTimeParam());
-					} catch (Exception e) {
-						sleepTime = 15L;
-					}
-					try {
-						Thread.sleep(TimeUnit.MINUTES.toMillis(sleepTime));
-					} catch (InterruptedException e) {
-						throw new RuntimeException(e);
-					}
-					settingsMap = ConfigReader.readConfig(appDir, studioLabel);
-					config = new Configuration(settingsMap);
-					needStop = Objects.equals("YES", settingsMap.get("NeedToStop"));
-				}
-				LocalDateTime date = LocalDateTime.now();
-				System.out.println(date + "Приложение завершило работу по команде из xml файла. " + studioLabel);
-			});
+			Thread t1 = new Thread(createTask(1, appDir));
+			Thread t2 = new Thread(createTask(2, appDir));
+			Thread t3 = new Thread(createTask(3, appDir));
 
-			Thread t2 = new Thread(() -> {
-				FileDistributor distributor = new FileDistributor();
-				String studioLabel = "Studio2";
-				Map<String, String> settingsMap = ConfigReader.readConfig(appDir, studioLabel);
-				Configuration config = new Configuration(settingsMap);
-				boolean needStop = false;
-				while (!needStop) {
-					long sleepTime;
-					System.out.println(LocalDateTime.now() + " Запущен процесс копирования " + studioLabel);
-					distributor.distribute(config.getBaseDir(), config.getNewDir());
-					System.out.println(LocalDateTime.now() + " Копирование завершено " + studioLabel);
-					try {
-						sleepTime = Long.parseLong(config.getSleepTimeParam());
-					} catch (Exception e) {
-						sleepTime = 15L;
-					}
-					try {
-						Thread.sleep(TimeUnit.MINUTES.toMillis(sleepTime));
-					} catch (InterruptedException e) {
-						throw new RuntimeException(e);
-					}
-					settingsMap = ConfigReader.readConfig(appDir, studioLabel);
-					config = new Configuration(settingsMap);
-					needStop = Objects.equals("YES", settingsMap.get("NeedToStop"));
-				}
-				LocalDateTime date = LocalDateTime.now();
-				System.out.println(date + "Приложение завершило работу по команде из xml файла. " + studioLabel);
-			});
+			List<Thread> tasks = List.of(t1, t2, t3);
+			for (Thread t : tasks) {
+				executorService.submit(t);
+			}
+			for (Thread t : tasks) {
+				t.join();
+			}
 
-			Thread t3 = new Thread(() -> {
-				FileDistributor distributor = new FileDistributor();
-				String studioLabel = "Studio3";
-				Map<String, String> settingsMap = ConfigReader.readConfig(appDir, studioLabel);
-				Configuration config = new Configuration(settingsMap);
-				boolean needStop = false;
-				while (!needStop) {
-					long sleepTime;
-					System.out.println(LocalDateTime.now() + " Запущен процесс копирования " + studioLabel);
-					distributor.distribute(config.getBaseDir(), config.getNewDir());
-					System.out.println(LocalDateTime.now() + " Копирование завершено " + studioLabel);
-					try {
-						sleepTime = Long.parseLong(config.getSleepTimeParam());
-					} catch (Exception e) {
-						sleepTime = 15L;
-					}
-					try {
-						Thread.sleep(TimeUnit.MINUTES.toMillis(sleepTime));
-					} catch (InterruptedException e) {
-						throw new RuntimeException(e);
-					}
-					settingsMap = ConfigReader.readConfig(appDir, studioLabel);
-					config = new Configuration(settingsMap);
-					needStop = Objects.equals("YES", settingsMap.get("NeedToStop"));
-				}
-				LocalDateTime date = LocalDateTime.now();
-				System.out.println(date + "Приложение завершило работу по команде из xml файла. " + studioLabel);
-			});
-			executorService.submit(t1);
-			executorService.submit(t2);
-			executorService.submit(t3);
-
-			t1.join();
-			t2.join();
-			t3.join();
 		} catch (Exception e) {
 			System.out.println(e.getMessage() + "\n" + e.getCause());
 			throw e;
 		}
 	}
+
+	private static Runnable createTask(int taskNumber, String appDir) {
+		return () -> {
+            FileDistributor distributor = new FileDistributor();
+            String studioLabel = "Studio" + taskNumber;
+            Map<String, String> settingsMap = ConfigReader.readConfig(appDir, studioLabel);
+            Configuration config = new Configuration(settingsMap);
+            boolean needStop = false;
+            while (!needStop) {
+                long sleepTime;
+                System.out.println(LocalDateTime.now() + " Запущен процесс копирования " + studioLabel);
+                distributor.distribute(config.getBaseDir(), config.getNewDir());
+                System.out.println(LocalDateTime.now() + " Копирование завершено " + studioLabel);
+                try {
+                    sleepTime = Long.parseLong(config.getSleepTimeParam());
+                } catch (Exception e) {
+                    sleepTime = 15L;
+                }
+                try {
+                    Thread.sleep(TimeUnit.MINUTES.toMillis(sleepTime));
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                settingsMap = ConfigReader.readConfig(appDir, studioLabel);
+                config = new Configuration(settingsMap);
+                needStop = Objects.equals("YES", settingsMap.get("NeedToStop"));
+            }
+            System.out.println(LocalDateTime.now() + "Приложение завершило работу по команде из xml файла. " + studioLabel);
+        };
+    }
 }
